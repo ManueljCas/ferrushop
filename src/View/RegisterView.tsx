028f596efa5a89e43c685e013b6eb3c6426c8611
ManuelCastro <jesus.castro.pech@gmail.com>
2024-05-15 02:14:16 -0500

    Nuevas Vistas Iniciar sesion y Registro

---
+++ src/View/RegisterView.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../Css/Login.css';
+
+const Login = () => {
+    return (
+        <div className="login-container">
+            <form className="login-form">
+                <h2>Registro</h2>
+                <h3>Bienvenido a Ferrushop</h3>
+                <div className="form-group">
+                    <input 
+                        type="email" 
+                        id="email" 
+                        name="email"
+                        placeholder="Correo Electrónico"
+                        required 
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password"
+                        placeholder="Contraseña"
+                        required 
+                    />
+                </div>
+                <h3>¿Haz olvidado tu contraseña? Haz clic <a href="hola">aquí</a></h3>
+                <button type="submit" className="login-button">Iniciar Sesión</button>
+                <h3>¿No tienes una cuenta? Haz clic <a href="hola">aquí</a></h3>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
